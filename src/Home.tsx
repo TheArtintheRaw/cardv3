95cb5946f3940a256dd60b4959596a925ed649d0
Swayze <theartintheraw@gmail.com>
2022-11-02 04:51:54 -0400

    bot tax

--- src/Home.tsx
+++ src/Home.tsx
@@ -118,7 +118,6 @@ export interface HomeProps {
 export type Guards = {
   address: PublicKey;
   botTax?: {
-    settings: BotTaxGuardSettings;
     payment?: SolAmount;
     lastInstruction?: boolean;
   };
